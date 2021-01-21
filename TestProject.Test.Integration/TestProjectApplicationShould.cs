c837034dc180ec98b4c93e5d3940291b49071d22
Vasyl Suddya <suddya@toptal.com>
2021-01-21 11:58:52 +0200

    PostUser success -> integration test

--- TestProject.Test.Integration/TestProjectApplicationShould.cs
+++ TestProject.Test.Integration/TestProjectApplicationShould.cs
@@ -1,4 +1,7 @@
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace TestProject.Test.Integration
@@ -22,5 +25,28 @@ namespace TestProject.Test.Integration
 
             Assert.Contains("{\"result\":[],\"totalCount\":0,\"pagingInfo\":{\"pageSize\":10,\"pageNumber\":0,\"sortDir\":null,\"sortKey\":null}}", responseString);
         }
+        
+          
+        [Fact]
+        public async Task PostUser()
+        {
+            HttpRequestMessage postRequest = 
+                new HttpRequestMessage(HttpMethod.Post, "api/user");
+
+            var obj = new {Email = "abc@2mail.com", MonthlySalary = 1211.50, MonthlyExpenses = 25};
+        
+            var json = JsonConvert.SerializeObject(obj);
+            
+            postRequest.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _fixture.Client.SendAsync(postRequest);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("{\"id\":\"1\",\"email\":\"abc@2mail.com\",\"monthlySalary\":1211.5,\"monthlyExpenses\":25}", responseString);
+
+
+        }
+        
     }
 }
